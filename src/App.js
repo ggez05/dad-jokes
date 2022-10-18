d05a8835638e4cd2e2a4d2281524f7bb1a6e2424
ggez05 <garvit510bhatia@gmail.com>
2022-10-18 17:36:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import NavBar from './components/navbar';
 import './App.css';
 
 function App() {
+  const URL = "https://official-joke-api.appspot.com/jokes/random";
+  const [jokes, setJokes] = useState("");
+  const [setup, setSetup] = useState("");
+  const [change, setChange] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(false);
+    const jokesgenerator = async () => {
+      const response = await fetch(URL).then((item) => item.json());
+      setJokes(response.punchline);
+      setSetup(response.setup);
+      setLoading(true);
+    }
+    jokesgenerator();
+  }, [change])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <NavBar />
+      <div className='container'>
+        <h1 className='heading'>Happy Laughing</h1>
+        {
+          loading ? (<div><h2 className='alert alert-primary'>{setup}</h2>
+            <h2 className='alert alert-info'>{jokes}</h2></div>)
+            : (<div className='alert alert-danger'>...Loading</div>)
+        }
+        <button className='btn btn-warning' onClick={() => setChange(!change)}>One More?</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div>
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                        DAD JOKES GENERATOR
+                </a>
+            </div>
+        </nav>
+        </div>
+    )
+}
+
+export default NavBar;
